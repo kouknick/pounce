b050e14b2fd588b5b84063fc73ed0f132712038e
kouknick <nick.koukounakis@runpanther.io>
2021-05-14 13:58:34 +0300

    chore: wip theme provider

--- src/utils/ThemeProvider.tsx
+++ src/utils/ThemeProvider.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import { ThemeProvider as EmotionThemeProvider } from '@emotion/react';
-import { theme as defaultTheme, Theme } from '../theme';
-import GlobalStyles from './GlobalStyles';
+import { Theme } from '../theme';
 
 interface ThemeProviderProps {
   /** The theme to pass down to the library components */
   theme?: Theme;
 }
 
-const ThemeProvider: React.FC<ThemeProviderProps> = ({ children, theme }) => (
-  <EmotionThemeProvider theme={theme || defaultTheme}>
-    <React.Fragment>
-      <GlobalStyles />
-      {children}
-    </React.Fragment>
-  </EmotionThemeProvider>
-);
+const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => <div>{children}</div>;
 
 export default ThemeProvider;
